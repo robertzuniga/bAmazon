06b1d91a36773bfdfa8b819566319d8d13c1383e
Robert Zuniga <github@mastertheday.com>
2019-08-04 03:59:51 -0400

    connection to bamazon database success

---
+++ bAmazonCustomer.js
@@ -0,0 +1,19 @@
+// Robert Zuniga
+var mysql = require("mysql");
+var inquire = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "bamazon"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id ==> " + connection.threadId);
+    connection.end();
+})
+
+// console.log(res);
